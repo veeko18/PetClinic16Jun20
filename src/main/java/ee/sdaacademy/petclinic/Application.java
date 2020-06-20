96b81a4915c71d14238be2e74497fb73fbb70d13
Veera Kondratenko <veeko2018@gmail.com>
2020-06-20 21:17:12 +0300

    consultant and owner scanner

--- src/main/java/ee/sdaacademy/petclinic/Application.java
+++ src/main/java/ee/sdaacademy/petclinic/Application.java
@@ -9,6 +9,25 @@ public class Application {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        System.out.println("Please enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Welcome to the Consultation room!");
+        System.out.println("You are starting consultation");
+        System.out.println("(1)  For consultation");
+        System.out.println("(-1) Exit");
+        System.out.print("Please input your choice: ");
+        int choice = scanner.nextInt();
+        if(choice == 1) {
+            System.out.println("Please, start the consultation ");
+        } else {
+            System.out.println("Exit");
+        }
+        while(scanner.hasNext())
+        {
+            System.out.println("Consultation Ended ");
+            break;
+        }
         /*PetTypeRepository petTypeRepository = new PetTypeRepository();
         petTypeRepository.findAll().stream().forEach(petTypes -> {
             System.out.println(petTypes.getPetTypeId() + " " + petTypes.getName());
